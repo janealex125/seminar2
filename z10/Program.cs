1093672f735e409315d223b1a4adbb3eb6208ae3
janealex125 <112669962+janealex125@users.noreply.github.com>
2022-09-07 10:51:38 +1000

    @

---
+++ z10/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int GenerateRandomNumbers()
+{
+    int n = new Random() .Next(100, 1000);
+    return n;
+}
+
+//процедура
+void CompraiseNumber()
+{
+    int n = GenerateRandomNumbers(); 
+    Console.WriteLine (n); 
+    int a = n / 10;
+ 
+      Console.WriteLine (a % 10);
+        
+}
+
+CompraiseNumber();
+
---
+++ z15/Program.cs
@@ -0,0 +1,11 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+Console.Clear();
+Console.Write("Введите число , обозначающее день недели, где 1 - понедельник, 7 - воскресенье: ");
+
+int dayOfWeek = int.Parse(Console.ReadLine()?? "0");
+
+if(dayOfWeek == 6 || dayOfWeek == 7)
+    Console.WriteLine("День недели является выходным днем.");
+
+else
+    Console.WriteLine("День не является выходным днем.");
